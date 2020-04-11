7f142db6b1dc2e26a402f278399da30ba73de9b1
achavesgs <alinecgsoares@hotmail.com>
2020-04-11 15:07:37 -0300

    rest template config

---
+++ src/main/java/com/example/ExemploMS/client/ClientImpl.java
@@ -0,0 +1,15 @@
+package com.example.ExemploMS.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ClientImpl {
+
+    Logger logger = LoggerFactory.getLogger(ClientImpl.class);
+
+    public String geraToken(){
+        logger.info("geraToken");
+
+        return "";
+    }
+}
--- src/main/java/com/example/ExemploMS/controller/Controller.java
+++ src/main/java/com/example/ExemploMS/controller/Controller.java
@@ -7,10 +7,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping(value = "v1")
 public class Controller {
 
     @Autowired
